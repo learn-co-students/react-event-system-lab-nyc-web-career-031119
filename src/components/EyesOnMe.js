9528945865877cfbb92faa5342d83ea8c32fff7a
Locallize <Fillanlee@aol.com>
2019-05-16 16:46:02 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+class EyesOnMe extends React.Component{
+
+    blurFunc = (e)=>{
+        console.log('Hey! Eyes on me!')
+    }
+    focusFunc =(e)=>{
+        console.log('Good!')
+    }
+
+
+    render(){
+        return <button onFocus={(e)=>this.focusFunc(e)} onBlur={(e)=> this.blurFunc(e)}>
+            "hello World"
+        </button>
+    }
+}
+export default EyesOnMe
